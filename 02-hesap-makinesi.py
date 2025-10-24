dc70c722e480b3f4a202bbf6ba4dc9522d42993c
Levent Durukan <84997585+levdur@users.noreply.github.com>
2025-10-24 17:03:08 +0300

    Update 02-hesap-makinesi.py

--- 02-hesap-makinesi.py
+++ 02-hesap-makinesi.py
@@ -1,1 +1,14 @@
 a = float(input("Birinci sayı: "))
+b = float(input("İkinci sayı: "))
+islem = input("İşlem (+, -, *, /): ")
+
+if islem == '+':
+    print(a + b)
+elif islem == '-':
+    print(a - b)
+elif islem == '*':
+    print(a * b)
+elif islem == '/':
+    print(a / b)
+else:
+    print("Geçersiz işlem!")
